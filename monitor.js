4656e26e42be8c11e527523af8118d7cd32aba7c
Daniel O'Connor <daniel.oconnor@gmail.com>
2014-02-09 01:51:08 +1030

    Add checks for comparison to outside temperature

--- monitor.js
+++ monitor.js
@@ -4,18 +4,31 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
 var util = require("util");
 var WebSocket = require('ws');
-
+var Forecast = require('forecast.io');
 var sqlite3 = require('sqlite3').verbose();
-// var db = new sqlite3.Database(':memory:');
+var fs = require('fs');
+var config = require('./config.js');
+var ws = new WebSocket('ws://' + config.host + ':8887/console');
+
+
+// https://api.forecast.io/forecast/053099b89f5af1170d940ff4a4d1a20f/-34.849482,138.521756
+function as_celcius(temp) {
+  return 5/9 * (temp - 32);
+}
+
+var options = {
+  APIKey: config.key,
+  requestTimeout: 1000
+},
+forecast = new Forecast(options);
+
+
+
 var db = new sqlite3.Database('temperature.sqlite3');
 
 db.run("CREATE TABLE IF NOT EXISTS climate_history (id INTEGER PRIMARY KEY, temperature NUMERIC,     goalTemperature NUMERIC,     name VARCHAR(255),     device_id VARCHAR(255), updated_at INTEGER)");
 
 
-
-
-// var ws = new WebSocket('ws://127.0.0.1:8887/console');
-var ws = new WebSocket('ws://192.168.1.7:8887/console');
 console.log("Created websocket.");
 
 ws.onopen = function(event) {
@@ -35,6 +48,7 @@ ws.onmessage = function(event) {
     // TODO Make more generic for any device/climate
     if (update["whatami"] == '/device/climate/samsung/control') {
       db.serialize(function () {
+        // Update everything
         stmt.run(update.info.temperature, update.info.goalTemperature, update.name, update.whoami, update.updated);
         // db.all("SELECT rowid AS id, temperature, updated_at FROM climate_history WHERE device_id = '" + update.whoami + '"', function(err, rows) {
         // db.all("SELECT rowid AS id, temperature, updated_at FROM climate_history", function(err, rows) {          
@@ -57,6 +71,22 @@ ws.onmessage = function(event) {
             console.log("Falling from " + rows[1].temperature + " to " + rows[0].temperature);
           }
 
+          // TODO This should probably push into steward via the simple reporting protocol majigger
+          forecast.get(config.lat, config.lon, function (err, res, data) {
+            if (err) throw err;
+            var outside_temperature = as_celcius(data.currently.temperature);
+            if (rows[0].temperature > outside_temperature) {
+              console.log("It's cooler outside: " + outside_temperature);
+            }
+            if (rows[0].temperature == outside_temperature) {
+              console.log("It's the same outside: " + outside_temperature);
+            }            
+            if (rows[0].temperature < outside_temperature) {
+              console.log("It's warmer outside: " + outside_temperature);
+            }
+          });
+
+
           // Then render a temperature chart
           var temps = [];
           rows.forEach(function (row) {
@@ -65,6 +95,8 @@ ws.onmessage = function(event) {
 
           console.log('https://chart.googleapis.com/chart?cht=lc&chs=200x100&chd=t:' + temps.join(",") + '&chxt=y&chds=a');
           //
+
+
 
         });
       });
